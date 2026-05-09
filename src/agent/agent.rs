8109e0786f9347f5a2ac3e26dccb559a063fb247
lgthevinh <everwellmax@gmail.com>
2026-05-10 02:15:10 +0700

    refactor: change agent struct fields to private visibility

--- src/agent/agent.rs
+++ src/agent/agent.rs
@@ -2,11 +2,11 @@ use super::agent_tool::AgentTool;
 use async_openai::{Client, config::OpenAIConfig};
 
 pub struct Agent {
-    pub id: String,
-    pub name: String,
-    pub system_instruction: String,
-    pub llm_client: Client<OpenAIConfig>,
-    pub tools: Vec<Box<dyn AgentTool>>,
+    id: String,
+    name: String,
+    system_instruction: String,
+    llm_client: Client<OpenAIConfig>,
+    tools: Vec<Box<dyn AgentTool>>,
 }
 
 impl Agent {
